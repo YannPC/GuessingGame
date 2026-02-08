0688583a338e9edb5317f7c19dc091d5c28e0fb6
masso <YannPC>
2026-02-08 14:06:02 +0100

    Add project files.

---
+++ GuessingGame/Game.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessingGame
+{
+    internal class Game
+    {
+        // add a private field to hold the secret number
+        private int _secretNumber;
+
+        // add a constructor that generates a random secret number between 1 and 100
+        public Game()
+        {
+            // use the Random class to generate a random number
+            Random generateRandomNumber = new Random();
+            _secretNumber = generateRandomNumber.Next(1, 15);
+        }
+
+
+
+        /*
+          Add a method CheckGuess(int guess) that returns:  "low" if guess is too low
+        , "high" if guess is too high, and "correct" if guess is correct.
+         */
+
+        public string CheckGuess(int guess) { 
+
+            if (guess < _secretNumber)
+            {
+                return "low";
+            }
+            else if (guess > _secretNumber)
+            {
+                return "high";
+            }
+            else
+            {
+                return "correct";
+            }
+
+        }
+
+
+
+
+    }
+}
---
+++ GuessingGame/Program.cs
@@ -0,0 +1,47 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Create an instance of Game.
+
+using GuessingGame;
+Game game = new Game();
+int numberOfGuesses = 0;
+string result = "";
+
+Console.WriteLine("Hello Gamer!");
+Console.WriteLine("What's your name Gamer");
+
+string gamerName = Console.ReadLine()!;
+
+Console.WriteLine($"Welcome {gamerName} to the Guessing Game!");
+
+Console.WriteLine("I have a secret number between 1 and 15. Can you guess it?");
+// Convert the input to an integer
+
+while (result != "correct")
+{
+    Console.WriteLine(" Enter your guess:");
+    string input = Console.ReadLine()!;
+
+    // Validate the input to ensure it's a valid integer
+
+    if (!int.TryParse(input, out int gamerGuessingNumber))
+    {
+        Console.WriteLine("Invalid input. Please enter a valid integer.");
+        continue; // Skip the rest of the loop and prompt again
+    }
+    numberOfGuesses++;
+    result = game.CheckGuess(gamerGuessingNumber);
+
+    if (result == "low")
+    {
+        Console.WriteLine("Your guess is too low. Try again!");
+    }
+    else if (result == "high")
+    {
+        Console.WriteLine("Your guess is too high. Try again!");
+    }
+     
+}
+    Console.WriteLine($"Congratulations {gamerName}! You've guessed the secret number in {numberOfGuesses} guesses!");
+
+
+
